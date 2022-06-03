1226cc0abd5f53fcbcd727bcbaadcc7c389c1c40
Richard Abhulimhen <richardabhu@fb.com>
2022-06-03 08:45:27 -0700

    Changed line 53 in mainactivity "Results" to "Result"

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
-                    Log.i(TAG,"Results: " + results.toString());
+                    Log.i(TAG,"Result: " + results.toString());
                     movies.addAll(Movie.fromJsonArray(results));
                     movieAdapter.notifyDataSetChanged();
                     Log.i(TAG,"Movies: " + movies.size());
